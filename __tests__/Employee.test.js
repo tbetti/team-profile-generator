13d7c741f49f83c7116e2a3e97ec55d95b65fbcc
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-12 13:29:14 -0400

    Added package.json file, included jest, wrote tests for Employee.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,44 @@
-const { hasUncaughtExceptionCaptureCallback } = require('process');
 const Employee = require('../lib/Employee.js');
 
-// describe('Employee class', () =>{
-//     it('Prompts user for name and returns name'){
-//         const name = 'Tiana';
+describe('Employee class', () =>{
+    describe('initialization', () =>{
+        it('should return an object containing name, id, and email', () => {
+            const name = 'Tiana';
+            const id = 1;
+            const email = 'email@email.com';
+            const employee = new Employee(name, id, email)
+    
+            expect(employee.name).toBe(name);
+            expect(employee.id).toBe(id);
+            expect(employee.email).toBe(email);
+        })
+    })
+    describe('getName function', () =>{
+        it('should return "Tiana"', () =>{
+            const employee = new Employee('Tiana', 1, 'email@email.com');
 
-//         expect(Employee.name).toBe(name); 
-//     }
-// })
+            expect(employee.getName()).toEqual('Tiana');
+        })
+    })
+    describe('getId function', () =>{
+        it('should return 1', () =>{
+            const employee = new Employee('Tiana', 1, 'email@email.com');
+
+            expect(employee.getId()).toEqual(1);
+        })
+    })
+    describe('getEmail function', () =>{
+        it('should return the email@email.com', () =>{
+            const employee = new Employee('Tiana', 1, 'email@email.com');
+
+            expect(employee.getEmail()).toEqual('email@email.com');
+        })
+    })
+    describe('getRole function', () =>{
+        it('should return "Employee"', () =>{
+            const employee = new Employee('Tiana', 1, 'email@email.com');
+
+            expect(employee.getRole()).toEqual('Employee');
+        })
+    })
+})
