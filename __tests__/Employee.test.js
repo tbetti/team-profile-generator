30d54b304b8b5506c6796b0c9e25a9afa7c0dfbe
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-12 13:43:02 -0400

    Created class for Employee.js that passed all jest tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,12 @@
 const Employee = require('../lib/Employee.js');
 
+const name = 'Tiana';
+const id = 1;
+const email = 'email@email.com';
+
 describe('Employee class', () =>{
     describe('initialization', () =>{
         it('should return an object containing name, id, and email', () => {
-            const name = 'Tiana';
-            const id = 1;
-            const email = 'email@email.com';
             const employee = new Employee(name, id, email)
     
             expect(employee.name).toBe(name);
@@ -15,28 +16,28 @@ describe('Employee class', () =>{
     })
     describe('getName function', () =>{
         it('should return "Tiana"', () =>{
-            const employee = new Employee('Tiana', 1, 'email@email.com');
+            const employee = new Employee(name, id, email);
 
             expect(employee.getName()).toEqual('Tiana');
         })
     })
     describe('getId function', () =>{
         it('should return 1', () =>{
-            const employee = new Employee('Tiana', 1, 'email@email.com');
+            const employee = new Employee(name, id, email);
 
             expect(employee.getId()).toEqual(1);
         })
     })
     describe('getEmail function', () =>{
         it('should return the email@email.com', () =>{
-            const employee = new Employee('Tiana', 1, 'email@email.com');
+            const employee = new Employee(name, id, email);
 
             expect(employee.getEmail()).toEqual('email@email.com');
         })
     })
     describe('getRole function', () =>{
         it('should return "Employee"', () =>{
-            const employee = new Employee('Tiana', 1, 'email@email.com');
+            const employee = new Employee(name, id, email);
 
             expect(employee.getRole()).toEqual('Employee');
         })
