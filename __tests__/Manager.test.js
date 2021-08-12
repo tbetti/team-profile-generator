329dc2bbcba1256b7156cd9735e9c88ccb5b40fd
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-12 14:44:18 -0400

    Added test for initialization of manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,26 @@
+const { it, expect } = require('@jest/globals');
+const { describe } = require('yargs');
 const Manager = require('../lib/Manager.js');
+
+const name = 'Tiana';
+const id = 1;
+const email = 'email@email.com';
+const officeNumber = 41;
+
+describe('Manager class', () =>{
+    describe('initialization', () =>{
+        const manager = new Manager(name, id, email, officeNumber);
+        it('should return an object extending the employee class with name', () =>{
+            expect(manager.name).toBe(name);
+        })
+        it('should return an object extending the employee class with id', () =>{
+            expect(manager.id).toBe(id);
+        })
+        it('should return an object extending the exployee class with email', ()=>{
+            expect(manager.email).toBe(email);
+        })
+        it('should return an object containing office number', () =>{
+            expect(manager.officeNumber).toBe(officeNumber);
+        })
+    })
+})
